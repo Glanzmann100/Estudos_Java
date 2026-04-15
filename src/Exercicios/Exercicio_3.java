ad6206400af28d361e553765a0a34dbf3374b75d
Matheus S Glanzmann <72605887+Glanzmann100@users.noreply.github.com>
2026-04-15 15:54:06 -0300

    Exercicio_3

---
+++ src/Exercicios/Exercicio_3.java
@@ -0,0 +1,14 @@
+package Exercicios;
+
+public class Exercicio_3 {
+    public static void main (String[] args) {
+        double baseMenor, baseMaior, altura, area;
+         baseMenor = 6;
+         baseMaior = 8;
+         altura = 5;
+
+        area = (baseMenor + baseMaior) / 2 * altura;
+        System.out.print(area);
+
+    }
+}
