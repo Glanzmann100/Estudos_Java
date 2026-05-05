7e53211cc1aee0d6a0790ad1343a608e864953f7
Matheus S Glanzmann <72605887+Glanzmann100@users.noreply.github.com>
2026-05-05 16:46:56 -0300

    Exercicio_13

---
+++ src/Exercicios/Exercicio_13.java
@@ -0,0 +1,21 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio_13 {
+    public static void main (String[] args) {
+        Scanner Telefonia = new Scanner(System.in);
+        int Minutos = 0;
+        double valor, valor_minuto = 2;
+
+        System.out.println("Quantos minutos de telefone você consumiu: ");
+        Minutos = Telefonia.nextInt();
+
+        if (Minutos <= 100) {
+            valor = 50;
+        } else {
+            valor = (((Minutos - 100) * 2) + 50);
+        }
+        System.out.printf("Valor a pagar: %.2f%n ", valor);
+    }
+}
