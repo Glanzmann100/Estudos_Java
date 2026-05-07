65057e2f54a49a0bfd27493994c9666f197c6f55
Matheus S Glanzmann <72605887+Glanzmann100@users.noreply.github.com>
2026-05-07 14:19:26 -0300

    Exercicio_16

---
+++ src/Exercicios/Exercicio_16.java
@@ -0,0 +1,16 @@
+package Exercicios;
+
+public class Exercicio_16 {
+    public static void main (String[] args) {
+        int x, y;
+
+        x = 5;
+        y = 0;
+
+        while (x > 2) {
+            System.out.print(x);
+            y = y + x;
+            x = x -1;
+        }
+    }
+}
