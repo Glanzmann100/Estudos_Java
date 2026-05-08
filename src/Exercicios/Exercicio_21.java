00006ed312ab071278f02add198112f5ea2416c2
Matheus S Glanzmann <72605887+Glanzmann100@users.noreply.github.com>
2026-05-08 00:24:42 -0300

    Exercicio_21

---
+++ src/Exercicios/Exercicio_21.java
@@ -0,0 +1,26 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio_21 {
+    public static void main (String[] args) {
+        Scanner contador_numeros = new Scanner(System.in);
+
+        int numero = contador_numeros.nextInt();
+        int dentro = 0;
+        int fora = 0;
+
+        for (int i = 0; i < numero; i++) {
+            int x = contador_numeros.nextInt();
+            if (x >= 10 && x <= 20) {
+                dentro++;
+            } else if (x <= 0) {
+                break;
+            } else {
+                fora++;
+            }
+        }
+        System.out.println(dentro + " in");
+        System.out.println(fora + " out");
+    }
+}
