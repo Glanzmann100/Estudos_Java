b84d0cca6002e00618518d9a378db9dbb274a9b1
Matheus S Glanzmann <72605887+Glanzmann100@users.noreply.github.com>
2026-05-12 15:06:08 -0300

    Aplicando uma classe Triangle com 3 atributos ao Exercicio_24

--- src/Exercicios/Exercicio_24.java
+++ src/Exercicios/Exercicio_24.java
@@ -1,5 +1,7 @@
 package Exercicios;
 
+import entities.Triangle;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -8,22 +10,26 @@ public class Exercicio_24 {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        Triangle x,y;
+        x = new Triangle();
+        y = new Triangle();
+
         System.out.println("Digite os valores do triangulo x: ");
-        double ax = sc.nextDouble();
-        double bx = sc.nextDouble();
-        double cx = sc.nextDouble();
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
 
         System.out.println("Digite os valores do triangulo y: ");
-        double ay = sc.nextDouble();
-        double by = sc.nextDouble();
-        double cy = sc.nextDouble();
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
 
-        double perimetro_x = (ax + bx + cx);
+        double perimetro_x = (x.a + x.b + x.c);
         double semiPerimetro_x = (perimetro_x / 2);
-        double perimetro_y = (ay + by + cy);
+        double perimetro_y = (y.a + y.b + y.c);
         double semiPerimetro_y = (perimetro_y / 2);
-        double area_X = Math.sqrt(semiPerimetro_x * (semiPerimetro_x - ax) * (semiPerimetro_x - bx) * (semiPerimetro_x - cx));
-        double area_y = Math.sqrt(semiPerimetro_y * (semiPerimetro_y - ay) * (semiPerimetro_y - by) * (semiPerimetro_y - cy));
+        double area_X = Math.sqrt(semiPerimetro_x * (semiPerimetro_x - x.a) * (semiPerimetro_x - x.b) * (semiPerimetro_x - x.c));
+        double area_y = Math.sqrt(semiPerimetro_y * (semiPerimetro_y - y.a) * (semiPerimetro_y - y.b) * (semiPerimetro_y - y.c));
 
         showResult(area_X, area_y);
     }
